6424b650a5e15cb242a8c51db3441bfed188d588
Zhibin Yu <zhibinyu97@gmail.com>
2024-02-12 18:16:13 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="landing">
+        <div className="greeting-line">My Name is&nbsp;</div>
+        <div className="greeting-line inverted">
+          <div className="typewriter">&nbsp;Shrimpkin&nbsp;</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
