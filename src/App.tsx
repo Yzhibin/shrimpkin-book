eab199ebcdbdd939e3dc753215615ed8b66b2d4c
Zhibin Yu <zhibinyu97@gmail.com>
2024-02-12 23:30:59 +0800

    swap en font family and list poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Landing from './components/Landing/Landing'
+import List from './components/List/List'
 
 function App() {
   return (
     <div>
       <Landing />
+      <List />
     </div>
   )
 }
---
+++ src/components/List/List.tsx
@@ -0,0 +1,12 @@
+import './List.css'
+
+function List() {
+  return (
+    <div className="list-container">
+      <div className="list-item">I Love Beijing</div>
+      <div className="list-item">no one on the street</div>
+    </div>
+  )
+}
+
+export default List
